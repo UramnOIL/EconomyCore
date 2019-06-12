ad11a458cb900dcb9ff8bc333c8b83112980ad24
uramnoil <uramnoil@outlook.jp>
2019-06-12 20:33:35 +0900

    Repositoryだけでいいかなって

--- src/main/kotlin/com/uramnoil/economycore/EconomyCore.kt
+++
@@ -1,9 +0,0 @@
-package com.uramnoil.economycore
-
-import com.uramnoil.economycore.repository.AccountRepository
-import com.uramnoil.economycore.repository.IAccountRepostirory
-import com.uramnoil.economycore.repository.dao.AccountMockDao
-
-class EconomyCore{
-    val repostirory: IAccountRepostirory = AccountRepository(AccountMockDao())
-}
